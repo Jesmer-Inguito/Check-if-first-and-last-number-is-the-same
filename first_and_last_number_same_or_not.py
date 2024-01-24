0d8109b09fc16cefd9222496f411423c17d5cd23
unknown <you@example.com>
2024-01-24 13:46:35 +0800

    created if-else with results

--- first_and_last_number_same_or_not.py
+++ first_and_last_number_same_or_not.py
@@ -5,9 +5,13 @@ numbers = [10, 20, 40, 50, 10]
 print("The numbers are:", numbers)
 
 # Check if first and last numbers are the same
+first_num = numbers[0]
+last_num = numbers[-1]
 
+if first_num == last_num:
 # Print results
-
+    print("The result is True")
 # Else
-
+else:
 # Print results
+    print("The result is False")
